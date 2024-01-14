54b098d3ee876b0418cb3ee0833e09517248db75
Sacha2023 <sachacruzabsi@gmail.com>
2024-01-14 17:15:00 +0100

    Test test avant ds

---
+++ src/main/java/org/centrale/medev_ds/DivisionByZeroException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.medev_ds;
+
+/**
+ *
+ * @author sacha
+ */
+public class DivisionByZeroException extends Exception {
+
+    public DivisionByZeroException(String division_by_zero_not_allowed) {
+        super();
+    }
+    
+}
