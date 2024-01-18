e067c24b1ffe34a60a404015732b9c542a63e324
Luan Lopes Fontes <l239682@dac.unicamp.br>
2024-01-18 08:06:11 +0100

    test sonar 3

--- src/main/java/org/centrale/medev_ds/DivisionByZeroException.java
+++ src/main/java/org/centrale/medev_ds/DivisionByZeroException.java
@@ -3,7 +3,7 @@ package org.centrale.medev_ds;
 public class DivisionByZeroException extends Exception {
 
     public DivisionByZeroException() {
-        super("Division by zero not allowed"); //en effet cest vrai
+        super("Division by zero not allowed"); //test
     }
 
     public DivisionByZeroException(String message) {
