61adca1fdb1aed53ab20901fbe64cd5e98d32475
Luan Lopes Fontes <l239682@dac.unicamp.br>
2024-01-18 09:54:45 +0100

    MaJ bateau

--- src/main/java/org/centrale/medev_ds/Bateau.java
+++ src/main/java/org/centrale/medev_ds/Bateau.java
@@ -24,14 +24,50 @@ public class Bateau {
         Coordenada coordenada = new Coordenada(x, y);
         coordenadas.add(coordenada);
     }
-    public void setPosition(int x_fin, int y_fin,int x_debut, int y_debut) {
-        
-       
+    public void setPosition(int x_debut, int y_debut,int x_fin, int y_fin) {
+        if (x_debut == x_fin) {
+            // Barco na orientação vertical
+            int minY = Math.min(y_debut, y_fin);
+            int maxY = Math.max(y_debut, y_fin);
+
+            for (int y = minY; y <= maxY; y++) {
+                adicionarCoordenada(x_debut, y);
+            }
+        } else if (y_debut == y_fin) {
+            // Barco na orientação horizontal
+            int minX = Math.min(x_debut, x_fin);
+            int maxX = Math.max(x_debut, x_fin);
+
+            for (int x = minX; x <= maxX; x++) {
+                adicionarCoordenada(x, y_debut);
+            }
+        } else {
+            // position invalide
+            System.out.println("le Bateau n'a pas une position valide");
+        }
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+
+    public List<Coordenada> getCoordenadas() {
+        return coordenadas;
+    }
+
+    public void setCoordenadas(List<Coordenada> coordenadas) {
+        this.coordenadas = coordenadas;
     }
     
     
+        
 }
-
+    
+      
 class Coordenada {
     private int x;
     private int y;
