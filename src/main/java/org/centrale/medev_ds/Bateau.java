4eeed609699e1449e2a0dba4696aed2d00916557
Sacha2023 <sachacruzabsi@gmail.com>
2024-01-18 09:57:21 +0100

    Tried tests with JUnit for preparation

--- src/main/java/org/centrale/medev_ds/Bateau.java
+++ src/main/java/org/centrale/medev_ds/Bateau.java
@@ -31,21 +31,3 @@ public class Bateau {
     
     
 }
-
-class Coordenada {
-    private int x;
-    private int y;
-
-    public Coordenada(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-}
